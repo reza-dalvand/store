cfce9460248dfedb74f3c1beb656330ee4c7e747
Reza <rdalvand@yahoo.com>
2023-11-18 12:19:49 +0330

    create contact us api

--- apps/api/v1/contact/api.py
+++ apps/api/v1/contact/api.py
@@ -1,26 +1,12 @@
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from apps.serializers.contact.serializers import ContactUsSerializer
 
-from apps.models.site.models import SiteSetting
-from apps.serializers import ContactUsSerializer
 
-
-class ContactUsView(GenericAPIView):
-    serializer_class = ContactUsSerializer
-
-    def get_queryset(self):
-        qs = SiteSetting.objects.filter(is_active=True).first()
-        return qs
-
+class ContactUsView(APIView):
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(
-                {"contact us": "successful register message"},
-                status=status.HTTP_201_CREATED,
-            )
-        return Response(
-            {"contact us": "Bad Request"}, status=status.HTTP_400_BAD_REQUEST
-        )
+        serializer = ContactUsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED)
