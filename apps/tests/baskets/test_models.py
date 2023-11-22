f83ef6ffff2fbeb8773c32f3aff28a13adbc343d
Reza <rdalvand@yahoo.com>
2023-11-22 11:49:12 +0330

    writing tests for model of basket app

--- apps/tests/baskets/test_models.py
+++ apps/tests/baskets/test_models.py
@@ -1,6 +1,6 @@
-from apps.tests.users.test_setup import SetupTest
+from apps.tests.baskets.test_setup import SetupTest
 
 
-# class ModelTest(SetupTest):
-#     def test_str_of_user_model(self):
-#         self.assertEquals(str(self.user), self.user.email)
+class ModelTest(SetupTest):
+    def test_str_basket_model(self):
+        self.assertEquals(str(self.basket), self.basket.user.get_full_name())
