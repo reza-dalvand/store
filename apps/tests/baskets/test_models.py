51f754fe4036d612445c30951cdeef0c9877c121
Reza <rdalvand@yahoo.com>
2023-11-23 13:07:51 +0330

    changes

--- apps/tests/baskets/test_models.py
+++ apps/tests/baskets/test_models.py
@@ -3,4 +3,5 @@ from .test_setup import SetupTest
 
 class ModelTest(SetupTest):
     def test_str_model(self):
+        print("Model")
         self.assertEquals(str(self.basket), self.basket.user.get_full_name())
