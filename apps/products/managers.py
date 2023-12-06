bacc3e8c26460b4a02aa4b6d39b4eccec2795c62
Reza <rdalvand@yahoo.com>
2023-12-06 20:04:37 +0330

    change product managers.py

--- apps/products/managers.py
+++ apps/products/managers.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class AdditionalQueryset(models.QuerySet):
-    """create new query set and chainable queries"""
+    """create new queryset and chainable queries"""
 
     def active_products(self, is_published=True, soft_deleted=False):
         return self.filter(is_published=is_published, soft_deleted=soft_deleted)
